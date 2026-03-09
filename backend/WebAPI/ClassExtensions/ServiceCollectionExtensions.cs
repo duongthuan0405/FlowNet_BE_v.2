4d049f5f34c260919f44919e99ee498c3421fdaf
duongthuan0405 <duongthuan0405@gmail.com>
2026-03-09 16:51:07 +0700

    FEAT: Swagegr Doc

--- backend/WebAPI/ClassExtensions/ServiceCollectionExtensions.cs
+++ backend/WebAPI/ClassExtensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace WebAPI.ClassExtensions
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebAPI.ClassExtensions
 {
     public static class ServiceCollectionExtensions
     {
@@ -9,7 +11,50 @@
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             services.AddOpenApi();
+
+            // Add Controllers
+            services.AddControllers();
+
+            AddSwagger(services);
             return services;
+        }
+
+        private static void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "FlowNet",
+                    Description = "A simple example ASP.NET Core Web API",
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Format: Bearer {your JWT token}"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
         }
     }
 }
--- backend/WebAPI/ClassExtensions/WebApplicationExtensions.cs
+++ backend/WebAPI/ClassExtensions/WebApplicationExtensions.cs
@@ -8,11 +8,15 @@
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            app.MapControllers();
 
             return app;
         }
